6e76a71c3feb7acb73086e6e4b8db0c5451b9899
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-09-21 20:23:15 +0200

    Corrected the script.js pathing to render

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/tasks';
+  var apiRoot = 'https://tasks-fl6a.onrender.com/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
